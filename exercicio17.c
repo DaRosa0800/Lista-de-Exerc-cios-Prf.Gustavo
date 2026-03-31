b66210c608d62e5e6165dbb7537058f7638b7ca1
DaRosa0800 <borel0060@gmail.com>
2026-03-31 07:29:12 -0300

    feito exercicios 16 e 17

---
+++ exercicio17.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+
+    char palavra[50];
+    int a = 0;
+
+    printf("Digite uma palavra");
+    scanf("%c", palavra);
+
+    for(int i = 0, palavra[1], i = "\0", i++);
+
+}
