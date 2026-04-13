e830d54dac048c9b5ad025cc75aa11448db0baeb
DaRosa0800 <borel0060@gmail.com>
2026-04-13 12:14:29 -0300

    atividades feitas

---
+++ lista 3/atividade1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+    
+    int numeros[5];
+    
+    for (int i = 0; i < 5; i++) {
+        
+        printf("Digite um numero: ");
+        scanf("%d", &numeros[i]);
+    
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        
+        printf("%d ", numeros[i]);
+    
+    }
+}
